96eb153e76b96e0927efe942bd27a6a410b73cf8
VridhiJ <vridhi@seas.upenn.edu>
2025-03-06 20:49:55 -0500

    training on random wind instead of fixed wind to avoid overfitting

--- rotorpy/examples/train_auto_eval.py
+++ rotorpy/examples/train_auto_eval.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import os
 from datetime import datetime
 from eval import evaluate_model
+from rotorpy.wind.dryden_winds import DrydenGust
 
 from rotorpy.vehicles.crazyflie_params import quad_params  # Import quad params for the quadrotor environment.
 
@@ -47,6 +48,13 @@ num_cpu = 4   # for parallelization
 # Choose the weights for our reward function. Here we are creating a lambda function over hover_reward.
 reward_function = lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5})
 
+def random_wind():
+    return DrydenGust(
+        dt=1/100,
+        sig_wind=np.random.uniform([25,25,0], [75,75,30]),  # Vary intensity
+        altitude=np.random.uniform(1.0, 2.0),
+    )
+
 # Make the environment. For this demo we'll train a policy to command collective thrust and body rates.
 # Turning render_mode="None" will make the training run much faster, as visualization is a current bottleneck.
 env = gym.make("Quadrotor-v0",
@@ -54,6 +62,7 @@ env = gym.make("Quadrotor-v0",
                 reward_fn = reward_function,
                 quad_params = quad_params,
                 max_time = 5,
+                wind_profile = random_wind(),
                 world = None,
                 sim_rate = 100,
                 render_mode='None')
